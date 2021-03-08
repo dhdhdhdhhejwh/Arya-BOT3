f9d3b3cde193bc63198bb07b825ba2b58ad991e6
dhdhdhdhhejwh <80098534+dhdhdhdhhejwh@users.noreply.github.com>
2021-03-08 10:54:28 +0700

    Update main.js

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ let rl = Readline.createInterface(process.stdin, process.stdout)
 let WAConnection = simple.WAConnection(_WAConnection)
 
 
-global.owner = ['6281515860089','6288235435804','6281357302007'] // Put your number here
+global.owner = ['6287871537199','994406705745','6281901668392'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
